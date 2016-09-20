57a094c5a5f3c063e5f519c94bcf859a92746948
Jimut <jemdhali67@gmail.com>
2016-09-20 21:31:01 +0530

    Setup Angular

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,13 +8,9 @@
 require('./bootstrap');
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
+ * Next, we will create a fresh Angular application instance.
+ * From here, you may begin adding components to the application,
+ * or feel free to tweak this setup for your needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: 'body'
-});
+const app = angular.module('CodeRunners', []);
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -11,13 +11,13 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 
 /**
- * Vue is a modern JavaScript library for building interactive web interfaces
- * using reactive data binding and reusable components. Vue's API is clean
- * and simple, leaving you to focus on building your next great project.
+ * Angular is a modern JavaScript library for building interactive web interfaces
+ * using two way data binding and reusable components. AngularJS lets you
+ * extend HTML vocabulary for your application. The resulting environment is
+ * extraordinarily expressive, readable, and quick to develop.
  */
 
-window.Vue = require('vue');
-require('vue-resource');
+window.angular = require('angular');
 
 /**
  * We'll register a HTTP interceptor to attach the "CSRF" header to each of
@@ -25,11 +25,11 @@ require('vue-resource');
  * included with Laravel will automatically verify the header's value.
  */
 
-Vue.http.interceptors.push((request, next) => {
-    request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
-
-    next();
-});
+// Vue.http.interceptors.push((request, next) => {
+//     request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
+//
+//     next();
+// });
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
